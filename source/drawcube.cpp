9445d6b90d8510173c5ca40a8a0a6f07db4ae86a
Filiph Sandström <filfat@hotmail.se>
2014-10-29 23:34:44 +0100

    Closing the shop for today

--- source/drawcube.cpp
+++ source/drawcube.cpp
@@ -112,7 +112,8 @@ void drawcube::drawcubeBlock(int x, int z, int y, GRRLIB_texImg* Block){
 
 /* Test Function to render blocks */
 void drawcube::drawcubeBlock(int x, int z, int y, GRRLIB_texImg* Block, bool t, bool bo, bool f, bool ba, bool l, bool r){
-	//Type:
+	//TODO: Correct the sides
+	
 	GRRLIB_3dMode(0.1, 1000, 45, 1, 0);
 	GRRLIB_SetTexture(Block,0);
 	GRRLIB_ObjectView(x,y,z, 0,0,0, 1,1,1);
