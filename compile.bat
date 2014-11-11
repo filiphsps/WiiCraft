ddf89819c12a1ec981183216f81821323d38680b
Filiph Sandström <filfat@hotmail.se>
2014-11-11 14:41:03 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,5 @@
+@ECHO OFF
+REM This file is used by the build server over at http://build.filfatstudios.com:8080 to build WiiCraft more easily
+REM Why you may ask? it was simpler to get it to work by using a separate BAT file ;)
 make clean
 make
